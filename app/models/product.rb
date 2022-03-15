66b35df43d1a8404f2c4abd1709903f34ebde538
Driganka <linkdriganka@gmail.com>
2022-03-15 17:04:43 +0530

    paginator, bootstrap & filevalidation implemented

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,4 @@
 class Product < ApplicationRecord
     has_many :product_images, dependent: :destroy
     accepts_nested_attributes_for :product_images, allow_destroy: true
-    validates :name, presence: true, length: {minimum: 2}
-    validates :code, presence: true
-    validates :images, file_size: { less_than: 1.megabytes }
 end
