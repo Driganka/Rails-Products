1d5e0e6e4aad332a3aebafb7079648767ecd2fad
Driganka <linkdriganka@gmail.com>
2022-03-15 17:03:43 +0530

    paginator, bootstrap & filevalidation implemented

--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -1,4 +1,7 @@
 class ProductImage < ApplicationRecord
     belongs_to :product
     mount_uploader :images, ImageUploader
+    validates :name, presence: true, length: {minimum: 2}
+    validates :code, presence: true
+    validates :images, file_size: { less_than: 1.megabytes }
 end
